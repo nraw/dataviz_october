accef7b8fa0a52a02e7d14097991242e50d364e3
nraw <marsic.andrej@gmail.com>
2019-10-09 22:23:18 +0200

    started advanced scraping

---
+++ ideas.md
@@ -0,0 +1,30 @@
+# Cool things to check
+
+## Basics
+
+### Metrics 
+Is there a rating trend over the years?
+
+Is there a jump count trend over the years?
+
+Is there a jump scare rating trend over the years?
+
+Best of each category
+
+Worst of each category
+
+### Correlations
+
+Does being on Netflix significantly affect any of the measures?
+
+Is there a correlation between the rating and jump count?
+
+Is there a correlation between jump count and jump rating?
+
+Is there a correlation between the rating and jump rating?
+What are the outliers?
+
+
+## Advanced
+
+Is there a 
--- scrape.py
+++ scrape.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import functools
+
+@functools.lru_cache()
+def get_soup(link):
+    res = requests.get(link)
+    soup = BeautifulSoup(res.content, 'lxml')
+    return soup
 
 res = requests.get('https://wheresthejump.com/full-movie-list/')
 soup = BeautifulSoup(res.content, 'lxml')
@@ -8,3 +15,15 @@ soup = BeautifulSoup(res.content, 'lxml')
 table = soup.find_all('table')[0]
 data = pd.read_html(str(table))[0]
 data.to_csv('jumpscares.csv')
+
+all_links = soup.find_all('a')  
+pattern = "https://wheresthejump.com/jump-scares"    
+good_links = [link.get('href') for link in all_links if pattern in link.get('href')]  
+good_links = good_links[1:]
+
+for link in good_links:
+    res = requests.get(link)
+    soup = BeautifulSoup(res.content, 'lxml')
+    content_part = soup.find('div', class_='entry-content')
+    content_part.find_all('p')[10]
+#TODO: make this a nice structure
